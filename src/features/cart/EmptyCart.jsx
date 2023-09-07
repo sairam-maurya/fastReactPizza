e5463520970d1ac437617a0642abad15c9109777
sairam-maurya <rams44258@gmail.com>
2023-09-07 12:38:36 +0530

    final commit

---
+++ src/features/cart/EmptyCart.jsx
@@ -0,0 +1,14 @@
+
+import LinkButton from '../../ui/LinkButton';
+
+function EmptyCart() {
+  return (
+    <div className='py-4 px-3'>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+
+      <p className='font-semibold mt-7'>Your cart is still empty. Start adding some pizzas :)</p>
+    </div>
+  );
+}
+
+export default EmptyCart;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,70 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       name: "chees",
+  //       quantity: 2,
+  //       unitPrice: 16,
+  //       totalPrice: 32,
+  //     },
+  //   ],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      //payload = new item
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      //payload = pizzaid
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQunatity(state, action) {
+      //payload = pizzaid
+
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  decreaseItemQuantity,
+  increaseItemQunatity,
+  deleteItem,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQunatity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
+//selector function like these can cause performnc issue so go see =>
+//reselect library
